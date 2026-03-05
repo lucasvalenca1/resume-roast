6811cec4c071c6ee7d335517f3c88d2259c78dfb
Lucas Valenca1 <133028118+lucasvalenca1@users.noreply.github.com>
2026-03-05 14:03:50 +0000

    Add serverComponentsExternalPackages to nextConfig

--- next.config.js
+++ next.config.js
@@ -2,18 +2,19 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: process.env.NEXT_DIST_DIR || '.next',
-    output: process.env.NEXT_OUTPUT_MODE,
-    experimental: {
-          outputFileTracingRoot: path.join(__dirname, '../'),
-    },
-    eslint: {
-          ignoreDuringBuilds: true,
-    },
-    typescript: {
-          ignoreBuildErrors: true,
-    },
-    images: { unoptimized: true },
+      distDir: process.env.NEXT_DIST_DIR || '.next',
+      output: process.env.NEXT_OUTPUT_MODE,
+      experimental: {
+              outputFileTracingRoot: path.join(__dirname, '../'),
+              serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+      },
+      eslint: {
+              ignoreDuringBuilds: true,
+      },
+      typescript: {
+              ignoreBuildErrors: true,
+      },
+      images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
