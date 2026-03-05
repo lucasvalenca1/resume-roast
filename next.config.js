e811c3965c9c33b45f1c35761822939f1227dcbb
Lucas Valenca1 <133028118+lucasvalenca1@users.noreply.github.com>
2026-03-05 14:24:30 +0000

    Refactor next.config.js for cleaner structure

--- next.config.js
+++ next.config.js
@@ -1,20 +1,12 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-      distDir: process.env.NEXT_DIST_DIR || '.next',
-      output: process.env.NEXT_OUTPUT_MODE,
-      experimental: {
-              outputFileTracingRoot: path.join(__dirname, '../'),
-              serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
-      },
-      eslint: {
-              ignoreDuringBuilds: true,
-      },
-      typescript: {
-              ignoreBuildErrors: true,
-      },
-      images: { unoptimized: true },
+        eslint: {
+                  ignoreDuringBuilds: true,
+        },
+        typescript: {
+                  ignoreBuildErrors: true,
+        },
+        images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
